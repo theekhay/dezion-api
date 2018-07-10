4e42fd446af9cd2c742e10d5de78b99f43bf8c7d
Ojo Tokunbo <tokunbo.ojo@talentbase.ng>
2018-07-10 18:42:53 +0100

    css fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,9 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if( env('APP_ENV') !== 'local')
-        {
-            $url->forceScheme('https');
+        if(env('APP_ENV') == 'production') {
+            \URL::forceScheme('https');
         }
 
         Schema::defaultStringLength(191) ;
